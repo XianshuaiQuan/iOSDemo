392a824040a50c079569bda4df0558067365af74
quanxianshuai <1570342684@qq.com>
2021-03-30 11:15:31 +0800

    AttributedString demo finished

---
+++ MTArributedString/MTAttributedString/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MTArributedString
+//
+//  Created by 全先帅 on 2021/3/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
