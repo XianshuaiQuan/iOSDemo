bbf611efbda48a543dbf45eb5b9e43632c2cc522
quanxianshuai <1570342684@qq.com>
2021-04-07 18:35:23 +0800

    摇奖机完成

---
+++ MTLotteryMachine/MTLotteryMachine/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MTLotteryMachine
+//
+//  Created by 全先帅 on 2021/4/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ MTLotteryMachine/MTLotteryMachine/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MTLotteryMachine
+//
+//  Created by 全先帅 on 2021/4/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
