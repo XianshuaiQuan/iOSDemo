5ad543af82673491fec5ba618a7dd0ba398470de
quanxianshuai <1570342684@qq.com>
2021-04-07 21:24:42 +0800

    消息机制/Rac替代消息 Demo 完成

---
+++ MTNSNotificationDemo/MTNSNotificationDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MTNSNotificationDemo
+//
+//  Created by 全先帅 on 2021/4/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MTNSNotificationDemo/MTNSNotificationDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MTNSNotificationDemo
+//
+//  Created by 全先帅 on 2021/4/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
